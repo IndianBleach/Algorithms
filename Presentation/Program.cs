e4e7cf28515c854b3ec636bdc6f74e44036b7780
indianbleach <alabamableach@gmail.com>
2022-03-02 23:15:56 +0300

    Добавьте файлы проекта.

---
+++ Presentation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Presentation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This project contains various thematic algorithm$");
+        }
+    }
+}
